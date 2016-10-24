63c62e2365ae6b41a2d930ebdc62e7ffa86b467f
gitford <ruffordpereira@gmail.com>
2016-10-24 10:58:12 +0530

    Create Library record works Properly,also primary keys are set

--- LibraryRecordSystemRam.java
+++ LibraryRecordSystemRam.java
@@ -18,9 +18,9 @@ public class LibraryRecordSystemRam {
 		{
 			Connection con=DriverManager.getConnection("jdbc:odbc:std");   //Choose as per convience!!
 			Statement st=con.createStatement();
-			String sql="create Table logininfo( LOGINID String(20), PASSWORD  String(20))";
-			String sql1="create Table studentinfo(STUDENTID int ,STUDENTNAME String(20),PHONENO int ,ADDRESS String(50),EMAIL String(30) )"; 
-			String sql2="create Table bookinfo(BOOKID int PRIMARY KEY ,BOOKNAME String(30),BOOKAUTHOR String(30),PUBLISIONS String(30))";
+			String sql="create Table logininfo([LOGINID] String(20)   PRIMARY KEY ,[PASSWORD]  String(20))";
+			String sql1="create Table studentinfo(STUDENTID int  PRIMARY KEY,STUDENTNAME String(20),PHONENO int ,ADDRESS String(50),EMAIL String(30))"; 
+			String sql2="create Table bookinfo(BOOKID int NOT NULL  PRIMARY KEY ,BOOKNAME String(30),BOOKAUTHOR String(30),PUBLISIONS String(30))";
 			String sql3="create Table transactioninfo(STUDENTID int ,STUDENTNAME String(20),BOOKID int ,BOOKNAME String(30),STARTDATE String(20) ,ENDDATE String(20))";
 			st.execute(sql);
 			st.execute(sql1);
